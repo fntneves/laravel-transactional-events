ae08565a1df9c6186421a5cf1f09ba0a9e116dcd
Francisco Neves <fntneves@gmail.com>
2018-04-14 12:21:52 +0100

    Clean trait

--- src/Neves/Testing/RefreshDatabase.php
+++ src/Neves/Testing/RefreshDatabase.php
@@ -2,12 +2,13 @@
 
 namespace Neves\Testing;
 
+namespace Illuminate\Events\Dispatcher;
 use Illuminate\Foundation\Testing\RefreshDatabase as BaseRefreshDatabase;
 
 trait RefreshDatabase
 {
     use BaseRefreshDatabase;
-    
+
     /**
      * Begin a database transaction on the testing database.
      *
@@ -15,60 +16,29 @@ trait RefreshDatabase
      */
     public function beginDatabaseTransaction()
     {
+        static $emptyDispatcher = new \Illuminate\Events\Dispatcher;
         $database = $this->app->make('db');
 
         foreach ($this->connectionsToTransact() as $name) {
             $connection = $database->connection($name);
 
-            // Disable event dispatcher in order to avoid event dispatching
-            // that could interfer with the behavior of the application
-            // under test. Enable it as soon as transaction begins.
-            $this->disableEventDispatcher($connection);
+            $currentDispatcher = $connection->getEventDispatcher();
+            $connection->setEventDispatcher($emptyDispatcher);
             $connection->beginTransaction();
-            $this->enableEventDispatcher($connection);
+            $connection->setEventDispatcher($currentDispatcher);
         }
 
-        $this->beforeApplicationDestroyed(function () use ($database) {
+        $this->beforeApplicationDestroyed(function () use ($database, $emptyDispatcher) {
             foreach ($this->connectionsToTransact() as $name) {
                 $connection = $database->connection($name);
 
-                // Disable event dispatcher in order to avoid event dispatching
-                // that could interfer with the behavior of the application
-                // under test. Enable as soon as transaction rollbacks.
-                $this->disableEventDispatcher($connection);
+                $currentDispatcher = $connection->getEventDispatcher();
+                $connection->setEventDispatcher($emptyDispatcher);
                 $connection->rollBack();
-                $this->enableEventDispatcher($connection);
+                $connection->setEventDispatcher($currentDispatcher);
 
                 $connection->disconnect();
             }
         });
-    }
-
-    /**
-     * Disable the event dispatcher of the given connection.
-     *
-     * @param  \Illuminate\Database\ConnectionInterface  $connection
-     * @return void
-     */
-    protected function disableEventDispatcher(ConnectionInterface $connection)
-    {
-        static $emptyDispatcher;
-
-        $emptyDispatcher = new \Illuminate\Events\Dispatcher;
-
-        $this->currentDisabledDispatcher = $connection->getEventDispatcher();
-        $connection->setEventDispatcher($emptyDispatcher);
-    }
-
-    /**
-     * Enable the event dispatcher of the given connection.
-     *
-     * @param  \Illuminate\Database\ConnectionInterface  $connection
-     * @return void
-     */
-    protected function enableEventDispatcher(ConnectionInterface $connection)
-    {
-        $connection->setEventDispatcher($this->currentDisabledDispatcher);
-        $this->currentDisabledDispatcher = null;
     }
 }
